ef75710efde7ddb91ac040a0e51e35a323834af0
clientesweb <alexisifran32@gmail.com>
2024-09-09 12:39:01 -0300

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Efecto de scroll suave al hacer clic en los enlaces de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
